2509ab3f21721931841f74d7183477a658cf670b
ktama <ktama@users.noreply.github.com>
2024-04-13 21:03:40 +0900

    プロジェクト ファイルを追加します。

---
+++ gRPC/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using gRPC;
+using Grpc.Core;
+using GrpcExample;
+
+Console.WriteLine("Setup Server...");
+var server = new Server
+{
+    Services = { Example.BindService(new gRpcServer()) },
+    Ports = { new ServerPort("127.0.0.1", 11111, ServerCredentials.Insecure) }
+};
+Console.WriteLine("Start Server...");
+server.Start();
+
+Console.WriteLine("Setup Client...");
+var chnnel = new Channel("127.0.0.1", 11111, ChannelCredentials.Insecure);
+var client = new Example.ExampleClient(chnnel);
+Console.WriteLine("Get MachineInfo...");
+var machineInfo = client.GetMachineInfo(new Google.Protobuf.WellKnownTypes.Empty());
+Console.WriteLine($"MachineName: {machineInfo.MachineName} / UserName: {machineInfo.UserName}");
+
+
+Console.WriteLine("Press any key to shutdown the server...");
+Console.ReadKey();
+
+server.ShutdownAsync().Wait();
---
+++ gRPC/gRpcServer.cs
@@ -0,0 +1,18 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcExample;
+
+namespace gRPC
+{
+    internal class gRpcServer : Example.ExampleBase
+    {
+        public override Task<ExampleReply> GetMachineInfo(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new ExampleReply
+            {
+                MachineName = Environment.MachineName,
+                UserName = Environment.UserName
+            });
+        }
+    }
+}
